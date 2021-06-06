180f0aba9554431720674e286a5a0023aab43be0
wujiahao.2021 <wujiahao.2021@bytedance.com>
2021-06-06 13:56:25 +0800

    添加缓冲区文件

--- include/log.h
+++ include/log.h
@@ -1,109 +1,271 @@
-#ifndef LOG_H_
-#define LOG_H_
+#ifndef _LOG_H__
+#define _LOG_H__
 
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/types.h> //getpid, gettid
 
-// 日志等级
 enum LOG_LEVEL
 {
     FATAL = 1,
-    ERROR,
-    WARN,
-    INFO,
-    DEBUG,
-    TRACE,
+    ERROR, //2
+    WARN,  //3
+    INFO,  //4
+    DEBUG, //5
+    TRACE, //6
 };
 
-// 自定义一个时间戳
+extern pid_t gettid();
+
 class utc_timer
 {
 public:
-    int year;
-    int mon;
-    int day;
-    int hour;
-    int min;
-    int sec;
-
-    utc_timer();
-    // 获取当前时间
-    uint64_t get_curr_time(int *p_msec = NULL);
+    int year, mon, day, hour, min, sec;
+    // char(19+'\0'):2021-06-06 13:12:28
+    char utc_fmt[20];
 
 private:
-    // 系统分钟时间
-    uint64_t sys_acc_min;
-    // 系统秒时间
-    uint64_t sys_acc_sec;
+    // 系统时间，单位分钟
+    uint64_t sys_min;
+    // 系统时间，单位秒
+    uint64_t sys_sec;
 
+public:
+    // 构造函数
+    utc_timer();
+    // 析构函数
+    ~utc_timer() {}
+    // 获取当前时间
+    uint64_t get_curr_time(int *p_msec = nullptr);
+
+private:
+    // 重置utc时间
     void reset_utc_fmt();
+
+    // 重置utc时间的秒部分
     void reset_utc_fmt_sec();
 };
 
-utc_timer::utc_timer()
+// 缓冲区
+class Buffer
 {
-    struct timeval tv;
-    // 获取系统时间（但是不陷入内核）
-    gettimeofday(&tv, NULL);
-    sys_acc_sec = tv.tv_sec;
-    sys_acc_min = sys_acc_sec / 60;
-    struct tm cur_tm;
-    localtime_r((time_t *)&sys_acc_sec, &cur_tm);
-    // 注意开始时间是1900
-    year = cur_tm.tm_year + 1900;
-    // 0-11
-    mon = cur_tm.tm_mon + 1;
-    day = cur_tm.tm_mday;
-    hour = cur_tm.tm_hour;
-    min = cur_tm.tm_min;
-    sec = cur_tm.tm_sec;
-    reset_utc_fmt();
-}
-
-uint64_t utc_timer::get_curr_time(int *p_msec = NULL)
-{
-    struct timeval tv;
-    // 先获取时间
-    gettimeofday(&tv, NULL);
-    if (p_msec)
+public:
+    enum buffer_status
     {
-        *p_msec = tv.tv_sec / 1000;
-    }
-    // 不是同一秒
-    if ((uint32_t)tv.tv_sec != sys_acc_sec)
+        FREE,
+        FULL
+    };
+
+    // 构造函数
+    Buffer(size_t len);
+
+    // 返回可用内存
+    size_t avail_len()
     {
-        sec = tv.tv_sec % 60;
-        // 更新秒
-        sys_acc_sec = tv.tv_sec;
-        // 不在同一分钟
-        if (sys_acc_sec / 60 != sys_acc_min)
-        {
-            // 更新分钟
-            sys_acc_sec = sys_acc_sec / 60;
-            struct tm cur_tm;
-            localtime_r((time_t *)&sys_acc_sec, &cur_tm);
-            year = cur_tm.tm_year + 1900;
-            mon = cur_tm.tm_mon + 1;
-            day = cur_tm.tm_mday;
-            hour = cur_tm.tm_hour;
-            min = cur_tm.tm_min;
-            sec = cur_tm.tm_sec;
-            reset_utc_fmt();
-        }
-        else
-        {
-            reset_utc_fmt();
-        }
+        return total_len - used_len;
     }
-    return tv.tv_sec;
-}
 
-void utc_timer::reset_utc_fmt()
-{
-}
+    // 判空
+    bool empty() const { return used_len == 0; }
 
-void utc_timer::reset_utc_fmt_sec()
+    // 添加内容
+    void append(const char *log_line, size_t len);
+
+    // 清空已有的内存
+    void clear();
+
+    // 持久化到磁盘文件中
+    void persist(FILE *fp);
+
+    buffer_status status;
+
+    Buffer *prev;
+    Buffer *next;
+
+private:
+    Buffer(const Buffer &);
+    Buffer &operator=(const Buffer &);
+
+    size_t total_len;
+    size_t used_len;
+    char *data;
+};
+
+class Log
 {
-}
+public:
+    // 获取唯一实例
+    static Log *getInstance()
+    {
+        static Log instance;
+        return &instance;
+    }
+
+    // 初始化，包括设置目录、进程名、日志等级
+    // 也会进行创建目录的操作
+    void init(const char *log_dir, const char *prog_name, int log_level);
+
+    // 获取日志等级
+    int get_level() const { return level; }
+
+    // 持久化
+    void persist();
+
+    // 向缓存链表中添加一条日志
+    void try_append(const char *lvl, const char *format, ...);
+
+private:
+    // 构造函数
+    Log();
+
+    // 判断当前文件属性决定是否要重新开一个文件
+    bool switch_file(int year, int mon, int day);
+
+    Log(const Log &);
+    const Log &operator=(const Log &);
+
+    int buffer_cnt;
+
+    Buffer *producer_ptr;
+    Buffer *consumer_ptr;
+    Buffer *last_buf;
+
+    FILE *fp;
+
+    // 进程id
+    pid_t pid;
+    // 当前正在写的日志文件的时间
+    int _year, _mon, _day;
+    // 当日日志文件个数
+    int log_count;
+    // 进程名
+    char prog[128];
+    // 目录
+    char dir[512];
+
+    // 是否当前设置的目录有写入权限
+    bool env_ok; //if log dir ok
+
+    int level;
+    // 上次限制写入日志的时间点
+    uint64_t last_limit_time;
+
+    utc_timer tm;
+
+    // 互斥锁
+    static pthread_mutex_t _mutex;
+    // 条件变量
+    static pthread_cond_t _cond;
+
+    // 一个缓存块的大小
+    static size_t buffer_block_size;
+};
+
+// 后台持久化日志的线程函数
+void *persist_worker(void *args);
+
+#define LOG_MEM_SET(mem_lmt)                   \
+    do                                         \
+    {                                          \
+        if (mem_lmt < 90 * 1024 * 1024)        \
+        {                                      \
+            mem_lmt = 90 * 1024 * 1024;        \
+        }                                      \
+        else if (mem_lmt > 1024 * 1024 * 1024) \
+        {                                      \
+            mem_lmt = 1024 * 1024 * 1024;      \
+        }                                      \
+        Log::_one_buff_len = mem_lmt;          \
+    } while (0)
+
+// 初始化日志类信息
+// 并且创建一个后台线程
+#define LOG_INIT(log_dir, prog_name, level)                  \
+    do                                                       \
+    {                                                        \
+        Log::getInstance()->init(log_dir, prog_name, level); \
+        pthread_t tid;                                       \
+        pthread_create(&tid, NULL, persist_worker, NULL);    \
+        pthread_detach(tid);                                 \
+    } while (0)
+
+// TRACE日志等级
+#define LOG_TRACE(fmt, args...)                                                                 \
+    do                                                                                          \
+    {                                                                                           \
+        if (Log::getInstance()->get_level() >= TRACE)                                           \
+        {                                                                                       \
+            Log::getInstance()->try_append("[TRACE]", "[%u]%s:%d(%s): " fmt "\n",               \
+                                           gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
+        }                                                                                       \
+    } while (0)
+
+// DEBUG日志等级
+#define LOG_DEBUG(fmt, args...)                                                                 \
+    do                                                                                          \
+    {                                                                                           \
+        if (Log::getInstance()->get_level() >= DEBUG)                                           \
+        {                                                                                       \
+            Log::getInstance()->try_append("[DEBUG]", "[%u]%s:%d(%s): " fmt "\n",               \
+                                           gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
+        }                                                                                       \
+    } while (0)
+
+// INFO日志等级
+#define LOG_INFO(fmt, args...)                                                                  \
+    do                                                                                          \
+    {                                                                                           \
+        if (Log::getInstance()->get_level() >= INFO)                                            \
+        {                                                                                       \
+            Log::getInstance()->try_append("[INFO]", "[%u]%s:%d(%s): " fmt "\n",                \
+                                           gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
+        }                                                                                       \
+    } while (0)
+
+// NORMAL日志等级
+#define LOG_NORMAL(fmt, args...)                                                                \
+    do                                                                                          \
+    {                                                                                           \
+        if (Log::getInstance()->get_level() >= INFO)                                            \
+        {                                                                                       \
+            Log::getInstance()->try_append("[INFO]", "[%u]%s:%d(%s): " fmt "\n",                \
+                                           gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
+        }                                                                                       \
+    } while (0)
+
+// WARN日志等级
+#define LOG_WARN(fmt, args...)                                                                  \
+    do                                                                                          \
+    {                                                                                           \
+        if (Log::getInstance()->get_level() >= WARN)                                            \
+        {                                                                                       \
+            Log::getInstance()->try_append("[WARN]", "[%u]%s:%d(%s): " fmt "\n",                \
+                                           gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
+        }                                                                                       \
+    } while (0)
+
+// ERROR日志等级
+#define LOG_ERROR(fmt, args...)                                                                 \
+    do                                                                                          \
+    {                                                                                           \
+        if (Log::getInstance()->get_level() >= ERROR)                                           \
+        {                                                                                       \
+            Log::getInstance()->try_append("[ERROR]", "[%u]%s:%d(%s): " fmt "\n",               \
+                                           gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
+        }                                                                                       \
+    } while (0)
+
+// FATAL日志等级
+#define LOG_FATAL(fmt, args...)                                                             \
+    do                                                                                      \
+    {                                                                                       \
+        Log::getInstance()->try_append("[FATAL]", "[%u]%s:%d(%s): " fmt "\n",               \
+                                       gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
+    } while (0)
 
 #endif
---
+++ test/test1/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../../include/log.h"
+
+using namespace std;
+
+int64_t get_current_millis(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (int64_t)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+int main()
+{
+    LOG_INIT("log", "test", 3);
+    uint64_t start_ts = get_current_millis();
+    for (int i = 0; i < 1e6; ++i)
+    {
+        LOG_ERROR("this test is no.%d", i);
+    }
+    uint64_t end_ts = get_current_millis();
+    printf("time use %llu\n", end_ts - start_ts);
+    return 0;
+}
